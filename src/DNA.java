820fb880dcf429558ef7587517c47531b0702347
Lucas Huang <lucas.huang@menloschool.org>
2025-10-19 13:11:28 -0700

    Finished linear search

--- src/DNA.java
+++ src/DNA.java
@@ -16,7 +16,19 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
+        int longest = 0;
 
-        return 0;
+        for (int i = 0; i < sequence.length(); i++) {
+            int count = 0;
+            while (i + (count + 1) * STR.length() <= sequence.length() &&
+                    sequence.substring(i + count * STR.length(), i + (count + 1) * STR.length()).equals(STR)) {
+                count++;
+            }
+
+            if (count > longest) {
+                longest = count;
+            }
+        }
+        return longest;
     }
 }
