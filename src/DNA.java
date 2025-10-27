6790015b4d5999f1cd30871a3901abe899ceb79a
Lucas Huang <lucas.huang@menloschool.org>
2025-10-27 16:58:14 -0700

    Finished

--- src/DNA.java
+++ src/DNA.java
@@ -12,7 +12,7 @@
 
 public class DNA {
 
-    private final static int RADIX = 256;
+    private final static int RADIX = 4;
     private final static long P_VALUE = 89434549747967L;
     public static int STRCount(String sequence, String STR) {
         int strLength = STR.length();
@@ -27,28 +27,36 @@ public class DNA {
         }
 
         int longest = 0;
-
-        for (int i = 0; i <= seqLength - strLength; i++) {;
-            int current = 0;
-            int currentIndex = i;
-            long tempHash = seqHash;
-
-            while (strHash == tempHash && !indexError(currentIndex, strLength, seqLength)) {
+        int current = 0;
+        int currentIndex = 0;
+        for (int i = 0; i <= seqLength - strLength;) {
+            if (strHash == seqHash) {
                 current++;
-                currentIndex += STR.length();
-                if (!indexError(currentIndex, STR.length(),sequence.length())) {
-                    tempHash = getHashCount(sequence.substring(currentIndex, currentIndex + STR.length()));
+                i += strLength;
+                if (!indexError(i, strLength,sequence.length())) {
+                    seqHash = getHashCount(sequence.substring(i, i + strLength));
                 }
             }
-
-            longest = compare(current, longest);
-
-            if (!indexError(i, STR.length() + 1, sequence.length())) {
-                char first = sequence.charAt(i);
-                char newest = sequence.charAt(i + STR.length());
-                seqHash = changeHashCount(seqHash, first, newest, highestRadix);
+            else {
+                if (current > 0) {
+                    longest = compare(current, longest);
+                    current = 0;
+                    i = i - strLength + 1;
+                    if (!indexError(i, STR.length(), sequence.length())) {
+                       seqHash = getHashCount(sequence.substring(i, i + strLength));
+                    }
+                }
+                else {
+                    i++;
+                    if (!indexError(i, STR.length(), sequence.length())) {
+                        char first = sequence.charAt(i - 1);
+                        char newest = sequence.charAt(i + STR.length() - 1);
+                        seqHash = changeHashCount(seqHash, first, newest, highestRadix);
+                    }
+                }
             }
         }
+        longest = compare(current, longest);
         return longest;
     }
 
@@ -61,7 +69,7 @@ public class DNA {
     }
 
     public static long changeHashCount(long seqHash, char first, char newest, long highestRadix) {
-        seqHash = (seqHash + P_VALUE - (first * highestRadix)) % P_VALUE;
+        seqHash = (seqHash + P_VALUE - ((first * highestRadix) % P_VALUE)) % P_VALUE;
         seqHash = (seqHash * RADIX + newest) % P_VALUE;
         return seqHash;
     }
